46d4b4ddc71d014afa01e08680b6b132fbbe558e
Mauricio Junior <mauricio.junior@gmail.com>
2018-04-21 20:00:28 -0300

    Update Program.cs

--- ConsoleAppCassandra/Program.cs
+++ ConsoleAppCassandra/Program.cs
@@ -11,23 +11,23 @@ namespace ConsoleAppCassandra
     {
         static void Main(string[] args)
         {
-            //Connect the demo keyspace on our cluster running at 127.0.0.1
+            //Conectando o keyspace com o cluster local
             Cluster cluster = Cluster.Builder().AddContactPoint("127.0.0.1").Build();
             ISession session = cluster.Connect("demo");
 
-            //insert data
+            //inserindo valores
             session.Execute("insert into users (lastName, firstName, age, city, email) values ('Junior', 'Mauricio', 35, 'Goiania', 'mauricio.junior@gmail.com')");
 
-            //reader
-            Row result = session.Execute("SELECT * FROM Users WHERE lastName = 'Junior'").First();
+            //lendo valores
+            Row result = session.Execute("SELECT lastName, fistName, age, city, email FROM Users WHERE lastName = 'Junior'").First();
             Console.WriteLine("{0} {1}", result["firstName"], result["age"]);
 
-            //update
+            //atualizando valores
             session.Execute("update Users set age = 36 where lastName = 'Junior'");
             result = session.Execute("select * from Users where lastName='Junior'").First();
             Console.WriteLine("{0} {1}", result["firstName"], result["lastName"]);
 
-            //delete
+            //deletando valores
             session.Execute("DELETE from Users where lastName='Junior'");
 
             RowSet rows = session.Execute("SELECT * FROM Users");
